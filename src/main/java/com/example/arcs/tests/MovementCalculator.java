405cc22b8b62e0ea8106bde21f2b51b969b020a9
Orthy <orthybt@gmail.com>
2023-07-13 23:30:10 +0200

    a

--- src/main/java/com/example/arcs/tests/MovementCalculator.java
+++ src/main/java/com/example/arcs/tests/MovementCalculator.java
@@ -1,3 +1,25 @@
 package com.example.arcs.tests;
 
-public class MovementCalculator {}
+import java.text.DecimalFormat;
+
+public class MovementCalculator {
+	private static DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+	public static double area(double base, double height) {
+		return (base * height) / 2.0;
+	}
+
+	public static double newHeight(double newBase, double area) {
+		return (2.0 * area) / newBase;
+	}
+
+	public static void main(String[] args) {
+		double baseArea = area(200, 50);
+		System.out.println("The area of the base triangle is: " + decimalFormat.format(baseArea));
+
+		double newBase = 100;
+		double newHeight = newHeight(newBase, baseArea);
+		System.out.println("The height for the new base " + decimalFormat.format(newBase) +
+				" to maintain the area is: " + decimalFormat.format(newHeight));
+	}
+}
